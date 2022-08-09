36845f0c1bf004b75f238f8e3e5bc9292d58b612
NguyenThai73 <nguyenvanthaind73@gmail.com>
2022-08-09 20:35:03 +0700

    update

---
+++ lib/screens/default_screen/default_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DefaultScreen extends StatelessWidget {
+  const DefaultScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 91, 189, 255),
+      body: Container(),
+    );
+  }
+}
